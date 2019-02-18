bee5fcad9dd76366cf3763ccf441ac7258b6316d
ripplejp <45304934+ripplejp@users.noreply.github.com>
2019-02-18 22:40:44 +0900

    Completed desktop and mobile header styles.

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -5,15 +5,19 @@ browserSync = require('browser-sync').create();
 gulp.task('watch', () =>{
     browserSync.init({
         notify: false,
-        server: "app"
+        server: "app",
     });
     gulp.watch('./app/index.html', () =>{
         browserSync.reload();
     }),
-    gulp.watch('./app/assets/styles/**/*.css',  gulp.series('cssInject'))
+    gulp.watch('./app/assets/styles/**/*.css',  gulp.series('cssInject')),
+    gulp.watch('./app/assets/scripts/**/*.js', gulp.series('scripts', () =>{
+        browserSync.reload();
+    }))
 });
 
 gulp.task('cssInject', gulp.series('styles' , () =>{
     return gulp.src('./app/temp/styles/styles.css')
     .pipe(browserSync.stream());
 }));
+
